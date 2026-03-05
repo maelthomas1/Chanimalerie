d9c4b6a7b67e466fa5edce6bdcf1312d55f98326
Maël Thomas <thomas-m02@saint-louis29.net>
2026-03-05 14:17:27 +0100

    Delete ChanimalerieAP_php directory

--- ChanimalerieAP_php/accueil.php
+++
@@ -1,8 +0,0 @@
-<div class="container my-5">
-    <!-- Titre en haut -->
-    <div class="bg-success bg-gradient text-white text-center rounded-3 p-5 mb-5 shadow">
-        <h1 class="display-4 fw-bold mb-3">
-            <i class="bi bi-star-fill"></i> Bienvenue à la Chatnimalerie <i class="bi bi-star-fill"></i>
-        </h1>
-    </div>
-</div>
